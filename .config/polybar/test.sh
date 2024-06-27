116549dd68d86e27a0f8279c2a26dbb7ef76a47c
4b-n4v <ab.navilon@gmail.com>
2024-06-27 18:55:55 -0500

    Removed test files lol

--- .config/polybar/test.sh
+++
@@ -1,9 +0,0 @@
-
-string1="hello"
-string2="2"
-
-if [[ "$(cat ~/mirror.txt | grep " connected" | cut -d "+" -f2 | uniq -c | cut -d " " -f7)" != "$string2" ]]; then
-  echo "Strings are equal"
-else
-  echo "Strings are not equal"
-fi
