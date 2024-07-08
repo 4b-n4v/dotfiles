0251b17070cd3e581a5993806d7baa4f73a626ce
4b-n4v <ab.navilon@gmail.com>
2024-07-08 09:05:55 -0500

    Clean up

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,6 @@
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload 4bn4v 
+		MONITOR=$m polybar --reload 4bn4v &
 	done
 else
 	polybar --reload 4bn4v &
