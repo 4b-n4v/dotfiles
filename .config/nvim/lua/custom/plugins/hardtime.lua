0d6e46e14c7ab258a2c632e7d264dd0f23cd618b
4b-n4v <ab.navilon@gmail.com>
2024-07-26 20:06:54 -0500

    rearrangement

--- .config/nvim/lua/custom/plugins/hardtime.lua
+++ .config/nvim/lua/custom/plugins/hardtime.lua
@@ -2,6 +2,7 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {
+		enabled = false,
 		disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
 	},
 }
